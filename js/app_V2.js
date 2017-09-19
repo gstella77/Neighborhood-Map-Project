bb4bae8300304845697a860afb0422a0205d4315
ggarcera <ggarcera@hotmail.com>
2017-09-19 11:42:46 -0600

    added animatedMarker function for fast user clicks

--- js/app_V2.js
+++ js/app_V2.js
@@ -8,47 +8,52 @@ MODEL
 
 var locations = [
     {title: 'Basque Block',
+    location: {
         lat: 43.613946,
         lng: -116.20246
+        }
     },
     {title: 'Gernika Bar',
-
+    location: {
         lat: 43.614072,
         lng: -116.202967
+        }
     },
     {title: 'Basque Museum',
-
+    location: {
         lat: 43.61382,
         lng: -116.202681
+        }
     },
     {title: 'Porton',
-
+    location: {
         lat: 43.613702,
         lng: -116.202606
-
+        }
     },
     {title: 'Boarding House',
-
+    location: {
         lat: 43.613655,
         lng: -116.202442
-
+        }
     },
     {title: 'Basque Center',
-
+    location: {
         lat: 43.613543,
         lng: -116.202293
-
+    }
     },
     {title: 'Basque Market',
-
-        lat:  43.614019,
+    location: {
+        lat: 43.614019,
         lng: -116.202074
-
+    }
     },
     {title: 'Leku ona Restaurant',
-
+    location: {
         lat: 43.614004,
         lng: -116.201861
+        }
     }
 ];
 
@@ -57,6 +62,8 @@ var map;
 // make infoWindow global so it closes when another marker is selected
 // https://discussions.udacity.com/t/closing-infowindow-when-i-open-another-one/288608
 var infowindow;
+
+//var bounds;
 
 /**************************
 Initialize Map
@@ -69,7 +76,23 @@ function initMap() {
     mapTypeControl: false
     });
 
-    this.infowindow = new google.maps.InfoWindow();
+    // Used infowindow construction
+    // Include third party API here
+    this.contentString =
+        '<h3>' + locations.title + '</h3>'+
+        '<div id="bodyContent">'+
+        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large sandstone rock formation in the southern part of the Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)</p>'+
+        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+        '(last visited June 22, 2009).</p>'+
+        '</div>'+
+        '</div>';
+
+    this.infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 300,
+        maxHeight: 150
+    });
 
     ko.applyBindings(new ViewModel());
 }
@@ -91,36 +114,30 @@ var MapLocation = function(data) {
 
     this.marker = new google.maps.Marker({
         map: map,
-        position: new google.maps.LatLng(self.lat(), self.lng()),
-        title: self.title(),
+        position: new google.maps.LatLng(data.location),
+        title: data.title,
         animation: google.maps.Animation.DROP,
     });
 
+    this.marker.setMap(map);
+
+    // This function executes only one bounce to prevent animation not
+    // stoping with fast user clicks
+    this.animateMarker = function(marker) {
+        if(this.marker != marker) {
+            this.marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function(marker){
+                self.marker.setAnimation(null);
+            }, 700);
+        }
+    };
+
+    // Animate and open infowindow
     this.marker.addListener('click', function(){
-        self.marker.setAnimation(google.maps.Animation.BOUNCE);
+        self.animateMarker();
         infowindow.open(map, this);
-
-        setTimeout(function(){
-            self.marker.setAnimation(null);
-        }, 700);
-
     });
 
-
-    // Used infowindow construction
-    // Include third party API here
-    this.contentString = '<h3>' + self.title() + '</h3>'+
-        '<div id="bodyContent">'+
-        '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large sandstone rock formation in the southern part of the Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi)</p>'+
-        '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-        'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-        '(last visited June 22, 2009).</p>'+
-        '</div>'+
-        '</div>';
-
-    //this.openwindow = new google.maps.InfoWindow({
-        //content: self.contentString
-   // });
 }
 
 /************************************
@@ -132,11 +149,11 @@ var ViewModel = function() {
 
     this.mapList = ko.observableArray([]);
 
+
     // iterate on each location and push MapLocation into mapList array
     // what does it do?
-    locations.forEach(function(placeItem){
+    locations.forEach(function(placeItem, marker){
         self.mapList.push( new MapLocation(placeItem));
-        //console.log(mapList() + 'loaded items');
     });
 
     // observe filter variable to determine its value in the ko.computed function below
@@ -146,7 +163,6 @@ var ViewModel = function() {
     indexOf method to check if no text is found in the filter.*/
     this.filteredItems = ko.computed(function() {
         var filter = self.filter().toLowerCase();
-
         return ko.utils.arrayFilter(self.mapList(), function(listItem) {
             var exist = listItem.title().toLowerCase().indexOf(filter) !== -1;
             if (listItem.marker) {
@@ -166,26 +182,18 @@ var ViewModel = function() {
     this.currentMap = ko.observable(this.mapList());
 
     // Click on current marker on the list and animate pin
-    this.getMarker = function(clickedMarker, marker, openwindow) {
+    this.getMarker = function(clickedMarker, marker) {
+
         self.currentMap(clickedMarker);
+
         if(self.currentMap().marker) {
-
-            this.marker.setAnimation(google.maps.Animation.BOUNCE);
-
+            this.animateMarker();
             //Open info window
             infowindow.open(map, this.marker);
-
             //this.marker.setVisible(false);
             console.log("item list clicked = " + self.currentMap().marker.title);
-        };
-
-        setTimeout(function(){
-            self.currentMap().marker.setAnimation(null);
-        }, 700);
-
+        }
     };
-
-
 };
 
 //ko.applyBindings(new ViewModel());
